39f6ffe94ed9f1e5dab8009778a391d7a2be4626
Rahul saini <rs7965498@gmail.com>
2023-10-26 18:34:22 +0530

    sum of natural nums

---
+++ sumOfNaturalNums.js
@@ -0,0 +1,17 @@
+// get input from the user.
+var num = 10;
+
+
+
+function sumOfNum(num){
+let sum = 0;
+    for(let i = 1; i<= num; i++){
+        sum += i
+    }
+
+    console.log(sum);
+
+}
+
+
+sumOfNum(num);
