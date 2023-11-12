c2ef8c4adefe8a815c5dd63cde6243631eb9fd29
Rahul saini <rs7965498@gmail.com>
2023-11-12 08:22:41 +0530

    count vowels

---
+++ countVowels.js
@@ -0,0 +1,29 @@
+// program to count the number of vowels in a string
+
+var prompt = require('prompt-sync')();
+
+// defining vowels
+const vowels = ["a", "e", "i", "o", "u"]
+
+function countVowel(str) {
+    // initialize count
+    let count = 0;
+
+    // loop through string to test if each character is a vowel
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+            // console.log(letter);
+        }
+    }
+
+    // return number of vowels
+    return count
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = countVowel(string);
+
+console.log(result);
