11b53c56e35491a284970cd3cc2fbe6263e16e27
Rahul saini <rs7965498@gmail.com>
2023-12-03 16:49:39 +0530

    first letter capital

---
+++ js38.js
@@ -0,0 +1,18 @@
+// program to convert first letter of a string to uppercase
+
+var prompt = require('prompt-sync')();
+
+function capitalizeFirstLetter(str) {
+
+    // converting first letter to uppercase
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return capitalized;
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = capitalizeFirstLetter(string);
+
+console.log(result);
