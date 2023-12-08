eee675a2d5c42d87f86357a564c144ecd1e3950d
Rahul saini <rs7965498@gmail.com>
2023-12-08 22:22:36 +0530

    add key/value pair to object

---
+++ dsa48.js
@@ -0,0 +1,16 @@
+// program to add a key/value pair to an object
+
+const person = {
+    name: 'Monica',
+    age: 22,
+    gender: 'female'
+}
+
+// add a key/value pair
+person.height = 5.4;
+
+// add a key/value pair
+person['qualification'] = 'graduate';
+
+
+console.log(person);
