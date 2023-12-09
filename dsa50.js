b318cf8325ddafd4c5a3142c1409fd3e6c78089c
Rahul saini <rs7965498@gmail.com>
2023-12-09 19:46:00 +0530

    multiline string

---
+++ dsa50.js
@@ -0,0 +1,21 @@
+// program to create a multiline strings
+
+// using the + operator
+const message1 = 'This is a long message\n' + 
+    'that spans across multiple lines\n' + 
+    'in the code.'
+
+    // using the \ operator
+const message2 = 'This is a long message\n \
+that spans across multiple lines\n \
+in the code.'
+
+// using the template literal
+
+const message3 = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
