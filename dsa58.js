1659fe7620f660011f82bbd24151a88ce5453759
Rahul saini <rs7965498@gmail.com>
2023-12-11 21:59:33 +0530

    string

--- dsa58.js
+++ dsa58.js
@@ -3,3 +3,8 @@
 const string = 'Learning JavaScript Program';
 const result = string.replace(/a/g, "A");
 console.log(result);
+
+
+const splitString = string.split('a');
+const result1 = splitString.join('A');
+console.log(result1);
