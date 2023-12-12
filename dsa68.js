94fe49fa22075e383c5766a9d86b3c5b66a92bfa
Rahul saini <rs7965498@gmail.com>
2023-12-12 12:27:01 +0530

    insert item an arr

--- dsa68.js
+++ dsa68.js
@@ -14,3 +14,28 @@ function insertElement() {
 }
 
 insertElement();
+
+
+//! program to insert an item at a specific index into an array using for loop:
+function insertElement() {
+    let array = [1, 2, 3, 4];
+
+    // index to add to
+    let index = 3;
+
+    // element that you want to add
+    let element = 8;
+  
+    for (let i = array.length; i > index; i--) {
+
+        //shift the elements that are greater than index
+        array[i] = array[i-1];
+    }
+
+    // insert element at given index
+    array[index] = element;
+
+    console.log(array);
+}
+
+insertElement();
