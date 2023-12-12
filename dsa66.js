2d714730274b0de569d56d64a24faa0a1eadf230
Rahul saini <rs7965498@gmail.com>
2023-12-12 12:27:43 +0530

    remove from arr

--- dsa66.js
+++ dsa66.js
@@ -17,21 +17,20 @@ const result = removeItemFromArray([1, 2, 3 , 4 , 5], 2);
 console.log(result);
 
 
-//!solution 2: 
+//!solution 2:
 // program to remove item from an array
 
 function removeItemFromArray(array, n) {
-    const index = array.indexOf(n);
+  const index = array.indexOf(n);
 
-    // if the element is in the array, remove it
-    if(index > -1) {
-
-        // remove item
-        array.splice(index, 1);
-    }
-    return array;
+  // if the element is in the array, remove it
+  if (index > -1) {
+    // remove item
+    array.splice(index, 1);
+  }
+  return array;
 }
 
-const result = removeItemFromArray([1, 2, 3 , 4, 5], 2);
+const result = removeItemFromArray([1, 2, 3, 4, 5], 2);
 
 console.log(result);
