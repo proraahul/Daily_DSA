ed94960cc90bec22b0d1d99e780e849f0a9f21fc
Rahul saini <rs7965498@gmail.com>
2023-12-13 21:30:54 +0530

    obj add in arr

---
+++ dsa69.js
@@ -0,0 +1,62 @@
+//!program to append an object to an array: Append Object to Array Using push()
+
+/*
+function insertObject(arr, obj) {
+
+    // append object
+    arr.push(obj);
+    
+    console.log(arr);
+}
+
+// original array
+let array = [1, 2, 3];
+
+// object to add
+let object = {x: 12, y: 8};
+
+// call the function
+insertObject(array, object);
+
+
+//!program to append an object to an array: Append Object to Array Using splice()
+
+
+function insertObject(arr, obj) {
+
+    // find the last index
+     let index = arr.length;
+ 
+     // appending object to end of array
+     arr.splice(index, 0, object);
+     
+     console.log(arr);
+ }
+ 
+ // original array
+ let array = [1, 2, 3];
+ 
+ // object to add
+ let object = {x: 12, y: 8};
+ 
+ // call the function
+ insertObject(array, object);
+*/
+ //!program to append an object to an array :Append Object Using Spread Operator 
+
+function insertObject(arr, obj) {
+
+    // append object
+     arr = [...arr, object];
+     
+     console.log(arr);
+ }
+ 
+ // original array
+ let array = [1, 2, 3];
+ 
+ // object to add
+ let object = {x: 12, y: 8};
+ 
+ // call the function
+ insertObject(array, object);
