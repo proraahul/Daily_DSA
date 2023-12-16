cbf28927e9aa81dbac6fa9e3f032d1f63a3b0e83
Rahul saini <rs7965498@gmail.com>
2023-12-16 17:25:09 +0530

    file extension

--- dsa80.js
+++ dsa80.js
@@ -5,11 +5,9 @@ function getFileExtension(filename){
     const extension = filename.split('.').pop();
     return extension;
 }
-
 // passing the filename
 const result1 = getFileExtension('module.js');
 console.log(result1);
-
 const result2 = getFileExtension('module.txt');
 console.log(result2);
 
@@ -22,9 +20,7 @@ function getFileExtension(filename){
     const extension = filename.substring(filename.lastIndexOf('.') + 1, filename.length);
     return extension;
 }
-
-const result1 = getFileExtension('module.js');
-console.log(result1);
-
-const result2 = getFileExtension('test.txt');
-console.log(result2);
+const result3 = getFileExtension('module.js');
+console.log(result3);
+const result4 = getFileExtension('test.txt');
+console.log(result4);
